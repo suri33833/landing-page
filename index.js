211ea60e6c91a110e6e732bbdee8d0af4c057b84
Surendra <sbora03sw@gmail.com>
2023-03-18 16:33:07 +0530

    updated Css and js

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ burger.addEventListener("click", () => {
   navList.classList.toggle("hidden-item");
   rightNav.classList.toggle("hidden-item");
 });
+
+function submitForm() {
+  let name =
+    document.querySelector("#firstName").value +
+    " " +
+    document.querySelector("#lastName").value;
+  let email = document.querySelector("#email").value;
+  let age = document.querySelector("#age").value;
+  let contact = document.querySelector("#phone").value;
+  alert(`
+  Name : ${name}
+
+  Email : ${email}
+
+  Phone Number : ${contact}
+
+  Age : ${age}`);
+}
